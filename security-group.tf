f9b11ab95153f61cc79270f64f2b83b831e9120e
ALFAHADH <s.alfahadh.472@gmail.com>
2026-01-14 14:29:29 +0530

    test

--- security-group.tf
+++ security-group.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 
   egress {
