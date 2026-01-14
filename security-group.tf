d8d16cb3b0d1fccfcacafdee6cc4a19faf3f6426
ALFAHADH <s.alfahadh.472@gmail.com>
2026-01-14 13:32:41 +0530

    changed the CIDR

--- security-group.tf
+++ security-group.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -20,7 +20,7 @@ resource "aws_security_group" "sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
